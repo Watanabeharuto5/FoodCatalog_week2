8f8f8b871ad9585524882962010316e1ce091ead
Watanabeharuto5 <karismaavibra@gmail.com>
2026-02-27 13:10:38 +0700

    State Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(
-    //membungkus aplikasi dengan ChangeNotifier agar state bisa di akses di mana saja
-    ChangeNotifierProvider(
+  runApp(ChangeNotifierProvider(
     create: (context) => CartModel(),
     child: MyApp(),
   ));
+}
 
-
+class CartModel extends ChangeNotifier {
+  final List<String> _items = [];
   
+  List<String> get items => _items;
+
+  void add(String itemName){
+    items.add(itemName);
+    notifyListeners();
+  }
+
+  void removeAll(){
+    items.clear();
+    notifyListeners();
+  }
 }
 
 
